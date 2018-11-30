82ad559479d0509b5dc9a23fd1f3673a268cae39
WallBall22 <44549704+WallBall22@users.noreply.github.com>
2018-11-30 11:05:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.user.setActivity("Kings",{type: 'WATCHING'})
-
 var prefix = "k!";
 client.on('message', message => {
     if (message.author.id === client.user.id) return;
     if (message.guild) {
    let embed = new Discord.RichEmbed()
     let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc1') {
+if(message.content.split(' ')[0] == prefix + 'bc') {
     if (!args[1]) {
 return;
 }
@@ -25,7 +23,19 @@ return;
         return;
     }
 });
+lient.on("message", message => {
 
+            if (message.content.startsWith(prefix + "obc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 
 
